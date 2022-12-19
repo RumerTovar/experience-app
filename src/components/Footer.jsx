a05333bc7249fa62133e1f665993cc744d653143
Rumer Tovar <rumertovar7474@gmail.com>
2022-12-19 12:26:24 -0500

    header logo and login buttom

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,17 +1,17 @@
-import React from "react";
-import "../styles.css"
+import React from 'react';
+import '../styles.css';
+import styles from './Header.module.css';
 
-const Footer = ({author}) => {
-    return(
-    <div className="footer-container">
-      <footer>
-        <span>
-          Design & illustrations by <span className='author-footer'>{author}</span>
-        </span>
-      </footer>
-    </div>
+const Footer = ({ author }) => {
+ return (
+  <div className='footer-container'>
+   <footer>
+    <span>
+     Design & illustrations by <span className='author-footer'>{author}</span>
+    </span>
+   </footer>
+  </div>
+ );
+};
 
-    )
-}
-
-export {Footer}
+export { Footer };
