7e129057c2318c4c05f2e40f86376906d38f402d
marin73tomas <marin73tomas@gmail.com>
2022-12-30 11:12:23 -0400

    responsive modal

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,49 +5,62 @@ import house from '../assets/images/icons/house.svg';
 import Hero from './Hero';
 
 const isEmpty = (obj) => {
- return Object.keys(obj).length === 0;
+    return Object.keys(obj).length === 0;
 };
 
 const Header = ({ setIsOpen, profile, setProfile }) => {
- const logOut = () => {
-  setProfile({});
- };
- return (
-  <div className={styles.headerWrapper}>
-   <header className={styles.headerContainer}>
-    <div className={styles.navContainer}>
-     <div className={styles.navLogoContainer}>
-      <div className={styles.imageContainer}>
-       <img src={experienceLogo} alt='logo' />
-      </div>
-      <span className={styles.navTitle}>Catalog</span>
-     </div>
-     {isEmpty(profile) ? (
-      <button className={styles.buttom}>
-       <img className={styles.houseImage} src={house} alt='house' />
-       <span onClick={() => setIsOpen(true)} className={styles.buttomText}>
-        login
-       </span>
-      </button>
-     ) : (
-      <button className={styles.buttom}>
-       <img className={styles.houseImage} src={house} alt='house' />
-       <span onClick={() => logOut()} className={styles.buttomText}>
-        {`${profile.given_name.charAt(0)} ${profile.family_name.charAt(0)}`}
-       </span>
-      </button>
-     )}
-    </div>
-    <Hero
-     img={logo}
-     author={'Luciano Polo'}
-     subjectType={'Painting'}
-     experience={'Create'}
-     gradeLevel={'9th'}
-    />
-   </header>
-  </div>
- );
+
+    const {
+        headerWrapper,
+        headerContainer,
+        navContainer,
+        navLogoContainer,
+        imageContainer,
+        navTitle,
+        buttom,
+        buttomText,
+        houseImage
+    } = styles
+
+    const logOut = () => {
+        setProfile({});
+    };
+    return (
+        <div className={headerWrapper}>
+            <header className={headerContainer}>
+                <div className={navContainer}>
+                    <div className={navLogoContainer}>
+                        <div className={imageContainer}>
+                            <img src={experienceLogo} alt='logo' />
+                        </div>
+                        <span className={navTitle}>Catalog</span>
+                    </div>
+                    {isEmpty(profile) ? (
+                        <button className={buttom}>
+                            <img className={houseImage} src={house} alt='house' />
+                            <span onClick={() => setIsOpen(true)} className={buttomText}>
+                                login
+                            </span>
+                        </button>
+                    ) : (
+                        <button className={buttom}>
+                            <img className={houseImage} src={house} alt='house' />
+                            <span onClick={() => logOut()} className={buttomText}>
+                                {`${profile.given_name.charAt(0)} ${profile.family_name.charAt(0)}`}
+                            </span>
+                        </button>
+                    )}
+                </div>
+                <Hero
+                    img={logo}
+                    author={'Luciano Polo'}
+                    subjectType={'Painting'}
+                    experience={'Create'}
+                    gradeLevel={'9th'}
+                />
+            </header>
+        </div>
+    );
 };
 
 export { Header };
